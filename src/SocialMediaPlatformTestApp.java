e6e2a5a363481801a017fb2f6b60adedcae7c25e
Bianca Staicu <biancastaicu_bella@yahoo.com>
2021-03-26 11:48:02 +0000

    test cause i am crazy

--- src/SocialMediaPlatformTestApp.java
+++ src/SocialMediaPlatformTestApp.java
@@ -21,7 +21,7 @@ public class SocialMediaPlatformTestApp {
 		System.out.println("Hello world");
 		System.out.println(" finally frdiday!");
 		System.out.println("The system compiled and started the execution...");
-
+		System.out.println("I gotta go to a festibal.");
 		SocialMediaPlatform platform = new BadSocialMedia();
 
 		assert (platform.getNumberOfAccounts() == 0) : "Innitial SocialMediaPlatform not empty as required.";
