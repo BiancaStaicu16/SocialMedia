6a761b5c8eab6a25f1d002239982f5c31127bcfa
Bianca Staicu <biancastaicu_bella@yahoo.com>
2021-03-29 14:10:29 +0100

    mcdonalds

--- src/SocialMediaPlatformTestApp.java
+++ src/SocialMediaPlatformTestApp.java
@@ -20,7 +20,9 @@ public class SocialMediaPlatformTestApp {
 	public static void main(String[] args) {
 		System.out.println("This is a piece of shit!!");
 		System.out.println("I want to cry");
+		System.out.println("I agree");
 		System.out.println("The system compiled and started the execution...");
+
 
 		SocialMediaPlatform platform = new BadSocialMedia();
 
