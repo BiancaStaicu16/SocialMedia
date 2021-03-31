82e6886d5468aca07b601dfd3cf2d041e044884e
Bh24 <bhasetty@gmail.com>
2021-03-31 10:02:16 +0100

    nothing much

--- src/socialmedia/Endorsement.java
+++ src/socialmedia/Endorsement.java
@@ -1,13 +1,35 @@
 package socialmedia;
 
 public class Endorsement extends Post{
+	
+	private int originalPostId;
+	
+	private int endorsementId;
 
-    public Endorsement(String message, String stringHandle) {
+	@Override
+    public Endorsement(String message, String stringHandle, int originalPostId) {
         super(message, stringHandle);
+        this.originalPostId = originalPostId;
     }
     
     public void setMessage(String message) {
     	super.setMessage(message);
+    }
+    
+    public int getOriginalPostId(){
+    	return originalPostId;
+    }
+    
+    public int getEndorsementId() {
+    	return endorsementId;
+    }
+    
+    public void setOriginalPostId(int id) {
+    	originalPostId = id;
+    }
+    
+    public void setEndorsementId(int id) {
+    	endorsementId = id;
     }
 
 }
