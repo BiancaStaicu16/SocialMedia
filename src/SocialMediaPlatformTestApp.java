8723ef2b1e4042775db7704152ae529df09bde40
Bianca Staicu <biancastaicu_bella@yahoo.com>
2021-04-15 13:30:32 +0100

    no

--- src/SocialMediaPlatformTestApp.java
+++ src/SocialMediaPlatformTestApp.java
@@ -18,7 +18,7 @@ public class SocialMediaPlatformTestApp {
 	 * Test method.
 	 * 
 	 * @param args not used
-	 * @throws IllegalHandleException 
+	 * @throws IllegalHandleException
 	 * @throws HandleNotRecognisedException
 	 * @throws NotActionablePostException 
 	 * @throws IOException 
@@ -27,7 +27,6 @@ public class SocialMediaPlatformTestApp {
 	public static void main(String[] args) throws HandleNotRecognisedException, IllegalHandleException, InvalidHandleException, InvalidPostException, PostIDNotRecognisedException, NotActionablePostException, IOException, ClassNotFoundException {
 
 		System.out.println("The system compiled and started the execution...");
-
 
 		SocialMediaPlatform platform = new SocialMedia();
 
@@ -38,21 +37,42 @@ public class SocialMediaPlatformTestApp {
 
 		Integer id;
 		Integer id1;
+		Integer id2;
+		Integer id3;
+		Integer id_of_post;
 		try {
+
+			// Creating a few accounts.
+
 			id = platform.createAccount("my_handle");
+			id1 = platform.createAccount("my_other_account", " my_description");
+			id2 = platform.createAccount("my_handle1");
+			id3 = platform.createAccount("my_other_account1", " my_description1");
 
-			assert (platform.getNumberOfAccounts() != 2) : "number of accounts registered in the system does not match";
-			System.out.println(platform.getNumberOfAccounts());
+			assert (platform.getNumberOfAccounts() == 4) : "number of accounts registered in the system does not match";
 
-			
-//			platform.removeAccount(id);
-			assert (platform.getNumberOfAccounts() == 0) : "number of accounts registered in the system does not match";
+			platform.removeAccount("my_handle1");
+			platform.removeAccount(id3);
 
-		} catch (IllegalHandleException e) {
+			assert (platform.getNumberOfAccounts() == 2): "number of accounts registered in the system does not match";
+
+			// Creating a few posts.
+
+			id_of_post = platform.createPost("my_handle", "a_message");
+
+			assert (platform.getTotalOriginalPosts() == 1) : "Innitial SocialMediaPlatform not empty as required.";
+
+			platform.commentPost("my_handle",id_of_post, "another_message");
+
+			assert (platform.getTotalCommentPosts() == 1) : "Innitial SocialMediaPlatform not empty as required.";
+
+			platform.endorsePost("my_handle", id_of_post);
+
+			assert (platform.getTotalEndorsmentPosts() == 1) : "Innitial SocialMediaPlatform not empty as required.";
+
+		} catch (IllegalHandleException | AccountIDNotRecognisedException e) {
 			assert (false) : "IllegalHandleException thrown incorrectly";
-//		 } catch (AccountIDNotRecognisedException e) {
-//			 assert (false) : "AccountIDNotRecognizedException thrown incorrectly";
-//		} catch (InvalidHandleException e) {
+		 } catch (InvalidHandleException e) {
 			e.printStackTrace();
 		}
 	}
